e57af1652d87a02825ea0784292de25cdd8f0e00
ChoWonJun999 <62541405+ChoWonJun999@users.noreply.github.com>
2024-07-26 18:39:26 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SET @TIME = -1;
+SELECT
+    @TIME := @TIME+1                    AS HOUR
+    , ( SELECT COUNT(*)
+        FROM ANIMAL_OUTS
+        WHERE HOUR(DATETIME) = @TIME
+    )                                   AS COUNT
+FROM ANIMAL_OUTS
+WHERE @TIME<23
